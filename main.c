d76f45041459c211ddbea9fc32a0ab6df51cf28f
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2018-10-22 14:03:57 -0300

    Verificando a existencia do arquivo contatos.txt e criado caso nao exista

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
  * - Validar ano da data_nasc
  * - Função de remover registro por string
  * - Função de visualizar registro por string
- * - Parte de arquivos
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -401,14 +401,26 @@ void carrega_contatos(Cabecalho *c){
   char Nome[101];
   char Telefone[11];
   char Endereco[101];
-  int CEP;
-  char DATA_NASC[11];
+  int Cep;
+  char Data_nasc[11];
+  
+  arquivo = fopen("contatos.txt", "r");
+
+  if(arquivo) {
+    fclose(arquivo);
+  }
+  
+  else{
+    arquivo = fopen("contatos.txt", "w");
+    fprintf(arquivo, "Tarra Casebolt\n96697-6327\n793 N. College St. La Vergne, TN\n37086\n26/09/1925\n$\nToney Schaffner\n95597-4268\n7596 East Vernon Drive Willingboro, NJ\n8046\n13/03/1936\n$\nMirna Halford\n97465-4294\n9841 Franklin St. Dalton, GA\n30721\n15/04/1951\n$\nBelva Yelvington\n99799-8331\n483 Roosevelt Street Ottumwa, IA\n52501\n25/09/1971\n$\nRodrigo Drews\n92130-3960\n10 Jockey Hollow Dr. Dothan, AL\n36301\n04/04/1994\n$\n");
+    fclose(arquivo);
+  }
   
   arquivo = fopen("contatos.txt", "r");
   
-  while((fscanf(arquivo, " %[^'\n'] %[^'\n'] %[^'\n'] %d %[^'\n'] %c\n", Nome, Telefone, Endereco, &CEP, DATA_NASC, &ch))!=EOF) {
+  while((fscanf(arquivo, " %[^'\n'] %[^'\n'] %[^'\n'] %d %[^'\n'] %c\n", Nome, Telefone, Endereco, &Cep, Data_nasc, &ch))!=EOF) {
 
-      carrega = cria_contato(Nome, Telefone, Endereco, CEP, DATA_NASC);
+      carrega = cria_contato(Nome, Telefone, Endereco, Cep, Data_nasc);
       insertion_sort_contato(c, carrega);    
 
   }
